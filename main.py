3b9ddf9483d182716b3e346c1675110aee6f4970
watchakorn-18k <74919942+watchakorn-18k@users.noreply.github.com>
2023-12-24 00:24:25 +0700

    chore: add code

---
+++ main.py
@@ -0,0 +1,16 @@
+# ข้อมูลความเร็วรถ
+speed_list = [100, 120, 140, 160, 180, 200, 220, 240, 260, 280]
+
+# ช่วงความเร็วที่ต้องการ
+min_speed = 210
+max_speed = 230
+
+# ข้อมูลรถตามความเร็ว
+car_name_list = ["Camry", "Corolla", "Prius", "Rav4","Supra", "Tundra", "Yaris", "Altima", "Sentra", "Titan"]
+
+
+
+
+
+
+# ผลลัพธที่ต้องการ ให้ print ออกมาให้ได้ Yaris 220 kmph
